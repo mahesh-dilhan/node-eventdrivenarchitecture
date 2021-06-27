a07db62dd931fc5a4a41d30adcc23d2264b8a642
mahesh <mail.mahesh.lk@gmail.com>
2021-06-27 18:32:01 +0800

    api/get

--- admin/src/app.ts
+++ admin/src/app.ts
@@ -1,8 +1,11 @@
 import * as express from 'express'
 import * as cors from 'cors'
 import {createConnection}  from 'typeorm'
+import {Product} from "./entity/product";
+import {json, Request, Response} from "express";
 
 createConnection().then(db=> {
+    const prodcutRepository = db.getRepository(Product)
     const app = express()
 
     app.use(cors({
@@ -12,6 +15,11 @@ createConnection().then(db=> {
 
     app.use(express.json())
 
+    app.get('/api/products', async (req: Request, res: Response) => {
+        const products = await prodcutRepository.find()
+        res.json(products);
+    });
+    
     console.log("listen 8080")
 
     app.listen(8080)
