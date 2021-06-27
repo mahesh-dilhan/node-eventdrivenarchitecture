dc28eec74e9fead22672542f3e2f3eb4b8338218
mahesh <mail.mahesh.lk@gmail.com>
2021-06-27 15:09:51 +0800

    typescript

--- admin/src/app.js
+++ admin/src/app.js
@@ -1,1 +1,11 @@
-console.log("event driven");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var cors = require("cors");
+var app = express();
+app.use(cors({
+    origin: ['http://localhost:4200', 'http://localhost:3000', 'http://localhost:8080']
+}));
+app.use(express.json());
+console.log("listen 8080");
+app.listen(8080);
--- admin/src/app.ts
+++ admin/src/app.ts
@@ -1,1 +1,15 @@
-console.log("event driven")
+import * as express from 'express'
+import * as cors from 'cors'
+
+const app = express()
+
+app.use(cors({
+    origin: ['http://localhost:4200', 'http://localhost:3000', 'http://localhost:8080']
+    }
+))
+
+app.use(express.json())
+
+console.log("listen 8080")
+
+app.listen(8080)
